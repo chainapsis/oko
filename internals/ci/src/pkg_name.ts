058d812db5a554217d85da3100cdfb2a68597b89
lidarbtc <lidarbtc@protonmail.com>
2025-11-07 12:47:50 +0900

    ci: fix build error on windows.

--- internals/ci/src/pkg_name.ts
+++ internals/ci/src/pkg_name.ts
@@ -1,12 +1,14 @@
 import path from "node:path";
 import fs from "node:fs";
+import { pathToFileURL } from "node:url";
 
 export async function getPkgName(pkg: string) {
   let name = path.parse(pkg).name;
 
   const pkgJsonPath = path.join(pkg, "package.json");
+  const url = pathToFileURL(pkgJsonPath).href;
   if (fs.existsSync(pkgJsonPath)) {
-    const pkgJson = await import(pkgJsonPath);
+    const pkgJson = await import(url);
     if (pkgJson.name) {
       name = pkgJson.name;
     }
