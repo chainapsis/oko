3a12f8a61287e9856e6a35fd79a7d7c5e9d587cc
blacktoast <dbstjd100@naver.com>
2025-11-12 16:47:33 +0900

    [demo_web] add isSignedInRef to track recent state in getKey callback

--- apps/demo_web/src/hooks/wallet.ts
+++ apps/demo_web/src/hooks/wallet.ts
@@ -1,31 +1,50 @@
 import { useSDKState } from "@oko-wallet-demo-web/state/sdk";
 import { useUserInfoState } from "@oko-wallet-demo-web/state/user_info";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export function useAddresses() {
   const okoCosmos = useSDKState((state) => state.oko_cosmos);
   const okoEth = useSDKState((state) => state.oko_eth);
   const isSignedIn = useUserInfoState((state) => state.isSignedIn);
+  const isSignedRef = useRef(isSignedIn);
+  isSignedRef.current = isSignedIn;
 
   const [cosmosAddress, setCosmosAddress] = useState<string | null>(null);
   const [ethAddress, setEthAddress] = useState<string | null>(null);
 
   useEffect(() => {
+    if (!isSignedIn) {
+      if (cosmosAddress) {
+        setCosmosAddress(null);
+      }
+
+      if (ethAddress) {
+        setEthAddress(null);
+      }
+      return;
+    }
+
     const loadAddresses = async () => {
       try {
         const promises = [];
 
         if (okoCosmos) {
           promises.push(
-            okoCosmos
-              .getKey("cosmoshub-4")
-              .then((key) => setCosmosAddress(key.bech32Address)),
+            okoCosmos.getKey("cosmoshub-4").then((key) => {
+              if (isSignedRef.current) {
+                setCosmosAddress(key.bech32Address);
+              }
+            }),
           );
         }
 
         if (okoEth) {
           promises.push(
-            okoEth.getAddress().then((addr) => setEthAddress(addr)),
+            okoEth.getAddress().then((addr) => {
+              if (isSignedRef.current) {
+                setEthAddress(addr);
+              }
+            }),
           );
         }
 
@@ -37,16 +56,6 @@ export function useAddresses() {
 
     if (isSignedIn) {
       loadAddresses();
-    }
-
-    if (!isSignedIn) {
-      if (cosmosAddress) {
-        setCosmosAddress(null);
-      }
-
-      if (ethAddress) {
-        setEthAddress(null);
-      }
     }
   }, [isSignedIn, okoCosmos, okoEth]);
 
