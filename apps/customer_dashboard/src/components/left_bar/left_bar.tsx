28557543bad2478cfcd5d50486a9795cf0b52eaf
lidarbtc <lidarbtc@protonmail.com>
2025-11-13 20:26:44 +0900

    o

--- apps/customer_dashboard/src/components/left_bar/left_bar.tsx
+++ apps/customer_dashboard/src/components/left_bar/left_bar.tsx
@@ -2,13 +2,11 @@
 
 import { MenuItem } from "@oko-wallet/oko-common-ui/menu";
 import { HomeOutlinedIcon } from "@oko-wallet/oko-common-ui/icons/home_outlined";
-import { Typography } from "@oko-wallet/oko-common-ui/typography";
 
 import { paths } from "@oko-wallet-ct-dashboard/paths";
 import styles from "./left_bar.module.scss";
 import { AccountInfoWithSubMenu } from "../account_info_with_sub_menu/account_info_with_sub_menu";
 import { ExternalLinkItem } from "../external_link_item/external_link_item";
-import { InfoModal } from "../info_modal/info_modal";
 
 export const LeftBar = () => {
   return (
