b2e1e83205c2e45d8a2c517efbd8d2653bc723f4
lidarbtc <lidarbtc@protonmail.com>
2025-11-21 16:56:23 +0900

    demo_web: remove beta status

---
+++ apps/demo_web/src/components/left_bar/integration_card/integration_card.tsx
@@ -0,0 +1,57 @@
+import { type FC } from "react";
+import { Typography } from "@oko-wallet/oko-common-ui/typography";
+import { CloseButtonIcon } from "@oko-wallet/oko-common-ui/icons/close_button_icon";
+
+import styles from "./integration_card.module.scss";
+
+interface IntegrationCardProps {
+  onClose: () => void;
+}
+
+export const IntegrationCard: FC<IntegrationCardProps> = ({ onClose }) => {
+  return (
+    <div className={styles.container}>
+      <button
+        className={styles.closeButton}
+        type="button"
+        aria-label="Close"
+        onClick={onClose}
+      >
+        <CloseButtonIcon size={12} />
+      </button>
+      <div className={styles.titleRow}>
+        <Typography size="sm" weight="semibold" color="primary">
+          Integrate Officially!
+        </Typography>
+      </div>
+      <Typography
+        size="sm"
+        weight="regular"
+        color="tertiary"
+        className={styles.description}
+      >
+        Get Oko’s managed infra and production support with official
+        integration.
+      </Typography>
+
+      <img
+        src={`${process.env.NEXT_PUBLIC_S3_BUCKET_URL}/assets/oko-demo-intergrate-officially.png`}
+        className={styles.image}
+      />
+
+      <div className={styles.getStartedButton}>
+        <Typography
+          tagType="a"
+          size="sm"
+          weight="semibold"
+          color="primary"
+          href={process.env.NEXT_PUBLIC_OKO_GET_SUPPORT_ENDPOINT}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Get Started →
+        </Typography>
+      </div>
+    </div>
+  );
+};
