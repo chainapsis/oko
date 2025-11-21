98e18552e22f36cffa2ab2ef8445178d2231c8e7
lidarbtc <lidarbtc@protonmail.com>
2025-11-21 13:48:09 +0900

    o

---
+++ apps/email_template/config.js
@@ -0,0 +1,25 @@
+/*
+|-------------------------------------------------------------------------------
+| Development config                      https://maizzle.com/docs/environments
+|-------------------------------------------------------------------------------
+|
+| This is the base configuration that Maizzle will use when you run commands
+| like `npm run build` or `npm run dev`. Additional config files will
+| inherit these settings, and can override them when necessary.
+|
+*/
+
+/** @type {import('@maizzle/framework').Config} */
+export default {
+  build: {
+    content: ["emails/**/*.html"],
+    static: {
+      source: ["images/**/*.*"],
+      destination: "images",
+    },
+  },
+  baseURL: {
+    url: "https://oko-wallet.s3.ap-northeast-2.amazonaws.com/",
+    tags: ["img"],
+  },
+};
