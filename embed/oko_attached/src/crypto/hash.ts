632e68669e93a65f8252b1032d0cce44d479977f
Calvin Lee <jinwoo0451@naver.com>
2025-11-26 16:05:07 +0900

    o

--- embed/oko_attached/src/crypto/hash.ts
+++ embed/oko_attached/src/crypto/hash.ts
@@ -14,7 +14,7 @@ export async function hashKeyshareNodeNames(
         err: hashResult.err,
       };
     }
-    const hash: Bytes32 = hashResult.data;
+    const hash = hashResult.data;
     const hashU8Arr = new Uint8Array(hash.toUint8Array());
     // 1 bytes prefix for compatibility with a range of a secret key(0, n);
     // n is the order of a elliptic curve
