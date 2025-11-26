b48495b36c6d3724bf18f960601f4315bfb0bf64
blacktoast <dbstjd100@naver.com>
2025-11-26 20:29:06 +0900

    [common] add OAuthProvider

--- common/oko_types/src/auth/index.ts
+++ common/oko_types/src/auth/index.ts
@@ -2,7 +2,7 @@ export interface TokenResult {
   token: string;
 }
 
-export type OAuthProvider = "google" | "auth0";
+export type OAuthProvider = "google" | "auth0" | "x";
 
 export type OAuthRequest<T> = {
   auth_type: OAuthProvider;
