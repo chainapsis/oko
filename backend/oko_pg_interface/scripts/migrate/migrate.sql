45f52c8f0c387c81f6e62caf4f2b6d92c423948d
Atmosis <jinwoo0451@naver.com>
2025-11-28 17:12:17 +0900

    oko_api, ks_node: Implement keypair-generation &key-rotation
    * api, pg_interface: impl key-gen/key-rotation
    
    * oko_api: refactor server keypair handling and add @oko-wallet/bytes dependency
    
    * Updated the server status route to return the public key from the keypair object.
    * Refactored server state management to use the EddsaKeypair type and return the keypair instead of just the public key.
    * Added @oko-wallet/bytes dependency to package.json and yarn.lock for enhanced byte manipulation functionality.
    
    * ks_node: implement ks_node's keypair management and rotation functionality
    
    * Added a new CLI command for rotating server keypairs.
    * Introduced functions for initializing, rotating, and deactivating server keypairs in the database.
    * Updated the server state to include the server keypair.
    * Enhanced encryption and decryption methods for keypair management.
    * Updated relevant routes to utilize the new keypair management features.
    * Added tests for the new keypair functionalities.
    
    Refs: BACG-362 [https://linear.app/keplrwallet/issue/BACG-362/cr-scheme-impl-keypair-generation-and-rotation]

--- backend/oko_pg_interface/scripts/migrate/migrate.sql
+++ backend/oko_pg_interface/scripts/migrate/migrate.sql
@@ -306,3 +306,24 @@ CREATE TABLE public.wallet_ks_nodes (
 );
 CREATE INDEX idx_wallet_ks_nodes_node_id ON public.wallet_ks_nodes USING btree (node_id);
 CREATE INDEX idx_wallet_ks_nodes_wallet_id ON public.wallet_ks_nodes USING btree (wallet_id);
+
+
+-- public.server_keypairs definition
+
+-- Drop table
+
+-- DROP TABLE public.server_keypairs;
+
+CREATE TABLE public.server_keypairs (
+	keypair_id uuid DEFAULT gen_random_uuid() NOT NULL,
+	version int4 GENERATED ALWAYS AS IDENTITY NOT NULL,
+	public_key bytea NOT NULL,
+	enc_private_key text NOT NULL,
+	is_active bool DEFAULT true NOT NULL,
+	created_at timestamptz DEFAULT now() NOT NULL,
+	updated_at timestamptz DEFAULT now() NOT NULL,
+	rotated_at timestamptz NULL,
+	CONSTRAINT server_keypairs_pkey PRIMARY KEY (keypair_id),
+	CONSTRAINT server_keypairs_version_key UNIQUE (version)
+);
+CREATE INDEX idx_server_keypairs_is_active ON public.server_keypairs USING btree (is_active) WHERE (is_active = true);
