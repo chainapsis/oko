89f257626e46703eb9eb7f1bceb76311af2c5e8c
Rita <editaahn@yahoo.com>
2025-11-28 22:24:10 +0900

    user_dashboard: update sign_in page layout and mock SignInImage

--- apps/user_dashboard/src/app/users/sign_in/page.tsx
+++ apps/user_dashboard/src/app/users/sign_in/page.tsx
@@ -1,15 +1,18 @@
 import styles from "./page.module.scss";
-import { SignInForm } from "@oko-wallet-user-dashboard/components/sign_in_form/sign_in_form";
 import { DashboardHeader } from "@oko-wallet-user-dashboard/components/dashboard_header/dashboard_header";
+import { SignInForm } from "@oko-wallet-user-dashboard/components/sign_in_form/sign_in_form";
+import { SignInImage } from "@oko-wallet-user-dashboard/components/sign_in_image/sign_in_image";
 
 export default function Page() {
   return (
     <div className={styles.wrapper}>
-      <DashboardHeader />
-
+      <DashboardHeader theme="dark" position="absolute" />
       <div className={styles.body}>
-        <div className={styles.formFrame}>
-          <SignInForm />
+        <SignInImage />
+        <div className={styles.content}>
+          <div className={styles.formFrame}>
+            <SignInForm />
+          </div>
         </div>
       </div>
     </div>
---
+++ apps/user_dashboard/src/components/sign_in_image/sign_in_image.tsx
@@ -0,0 +1,26 @@
+import { Typography } from "@oko-wallet-common-ui/typography/typography";
+
+import styles from "./sign_in_image.module.scss";
+
+export const SignInImage = () => (
+  <div
+    className={styles.container}
+    style={{
+      backgroundImage: `url(${process.env.NEXT_PUBLIC_S3_BUCKET_URL}/assets/oko-user-dashboard-login.png)`,
+    }}
+  >
+    <Typography tagType="h2" size="display-lg" weight="bold" color="white">
+      Oko Home
+    </Typography>
+    <Typography
+      tagType="p"
+      size="lg"
+      weight="bold"
+      color="white"
+      className={styles.description}
+    >
+      Everything you hold, all in one place. <br />
+      Check your balances, addresses, <br /> and connected apps at a glance.
+    </Typography>
+  </div>
+);
