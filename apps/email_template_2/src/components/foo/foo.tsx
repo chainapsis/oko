4769a64041fbdd7e5984efc6126dbc1e9273c8f1
Elden Park <eldeniyenden@gmail.com>
2025-12-01 23:12:27 -0800

    o

--- apps/email_template_2/src/components/foo/foo.tsx
+++ apps/email_template_2/src/components/foo/foo.tsx
@@ -1,7 +1,7 @@
 import type { CSSProperties, FC } from "react";
 
 export const Foo: FC = () => {
-  const style: CSSProperties = { color: "blue" };
+  const style: CSSProperties = { backgroundColor: "blue" };
 
   return <div style={style}>foo</div>;
 };
