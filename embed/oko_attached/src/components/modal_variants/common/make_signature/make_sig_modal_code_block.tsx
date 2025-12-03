f30a6626dc8a2bad4d0fb93b5d36110cbdc7f37e
devmosis <ryzond6728@gmail.com>
2025-12-03 19:13:08 +0900

    oko_attached: optimize MakeSignatureRawCodeBlock component with useMemo for code formatting

--- embed/oko_attached/src/components/modal_variants/common/make_signature/make_sig_modal_code_block.tsx
+++ embed/oko_attached/src/components/modal_variants/common/make_signature/make_sig_modal_code_block.tsx
@@ -3,23 +3,25 @@ import { CodeBlock } from "@oko-wallet-attached/components/code_block/code_block
 import cn from "classnames";
 
 import styles from "./make_sig_modal_code_block.module.scss";
+import { useMemo, type FC } from "react";
 
 interface MakeSignatureRawCodeBlockProps {
   code: string;
   className?: string;
 }
 
-export const MakeSignatureRawCodeBlock: React.FC<
-  MakeSignatureRawCodeBlockProps
-> = ({ code, className }) => {
-  const formattedCode = (() => {
+export const MakeSignatureRawCodeBlock: FC<MakeSignatureRawCodeBlockProps> = ({
+  code,
+  className,
+}) => {
+  const formattedCode = useMemo(() => {
     try {
       const parsed = JSON.parse(code);
       return JSON.stringify(parsed, null, 2);
     } catch {
       return code;
     }
-  })();
+  }, [code]);
 
   return (
     <CodeBlock
