d78fd07f5c307f5cb4541578c4bbd62e3de21410
chihunmanse <chihunmanse@gmail.com>
2025-12-04 14:30:53 +0900

    o

--- key_share_node/server/src/auth/auth0/client_id.ts
+++ key_share_node/server/src/auth/auth0/client_id.ts
@@ -1,2 +1,3 @@
+// @TODO: change to the actual domain & client id
 export const AUTH0_DOMAIN = "dev-0v00qjwpomau3ldk.us.auth0.com";
 export const AUTH0_CLIENT_ID = "AMtmlNKxJiNY7abqewmq9mjERf2TOlfo";
