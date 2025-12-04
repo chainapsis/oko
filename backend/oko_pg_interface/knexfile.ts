e9fe8cf1be2f6575a618dc85c0f4267b56b7e7ba
lidarbtc <lidarbtc@protonmail.com>
2025-12-04 18:12:28 +0900

    oko_pg_interface: add knex schema

--- backend/oko_pg_interface/knexfile.ts
+++ backend/oko_pg_interface/knexfile.ts
@@ -4,29 +4,6 @@ import type { Knex } from "knex";
 // Update with your config settings.
 
 const config: { [key: string]: Knex.Config } = {
-  // development: {
-  //   client: "sqlite3",
-  //   connection: {
-  //     filename: "./dev.sqlite3"
-  //   }
-  // },
-  //
-  // staging: {
-  //   client: "postgresql",
-  //   connection: {
-  //     database: "my_db",
-  //     user: "username",
-  //     password: "password"
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: "knex_migrations"
-  //   }
-  // },
-
   // production: {
   //   client: "postgresql",
   //   connection: {
@@ -46,7 +23,6 @@ const config: { [key: string]: Knex.Config } = {
   development: {
     client: "pg",
     connection: {
-      // TODO: @hyunjae
       host: "localhost",
       user: "your_user",
       password: "your_password",
