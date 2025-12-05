899f81ff4d5d572cfe8a8a790c10442cdc694129
chihunmanse <chihunmanse@gmail.com>
2025-12-05 17:42:19 +0900

    o

--- key_share_node/pg_interface/migrations/initial.ts
+++ key_share_node/pg_interface/migrations/initial.ts
@@ -4,183 +4,160 @@ export async function up(knex: Knex): Promise<void> {
   //
   // public.key_shares
   //
-  await knex.schema
+  const keySharesExists = await knex.schema
     .withSchema("public")
-    .hasTable("key_shares")
-    .then((exists) => {
-      if (!exists) {
-        return knex.schema
-          .withSchema("public")
-          .createTable("key_shares", (table) => {
-            table
-              .uuid("share_id")
-              .notNullable()
-              .defaultTo(knex.raw("gen_random_uuid()"))
-              .primary({ constraintName: "key_shares_pkey" });
-            table.uuid("wallet_id").notNullable();
-            table.binary("enc_share").notNullable();
-            table.string("status").notNullable();
-            table
-              .timestamp("reshared_at", { useTz: true })
-              .notNullable()
-              .defaultTo(knex.fn.now());
-            table
-              .timestamp("created_at", { useTz: true })
-              .notNullable()
-              .defaultTo(knex.fn.now());
-            table
-              .timestamp("updated_at", { useTz: true })
-              .notNullable()
-              .defaultTo(knex.fn.now());
-            table.jsonb("aux");
+    .hasTable("key_shares");
+  if (!keySharesExists) {
+    await knex.schema
+      .withSchema("public")
+      .createTable("key_shares", (table) => {
+        table
+          .uuid("share_id")
+          .notNullable()
+          .defaultTo(knex.raw("gen_random_uuid()"))
+          .primary({ constraintName: "key_shares_pkey" });
+        table.uuid("wallet_id").notNullable();
+        table.binary("enc_share").notNullable();
+        table.string("status").notNullable();
+        table
+          .timestamp("reshared_at", { useTz: true })
+          .notNullable()
+          .defaultTo(knex.fn.now());
+        table
+          .timestamp("created_at", { useTz: true })
+          .notNullable()
+          .defaultTo(knex.fn.now());
+        table
+          .timestamp("updated_at", { useTz: true })
+          .notNullable()
+          .defaultTo(knex.fn.now());
+        table.jsonb("aux");
 
-            table.unique(["wallet_id"], {
-              indexName: "key_shares_unique",
-            });
-          });
-      }
-      return Promise.resolve();
-    });
+        table.unique(["wallet_id"], {
+          indexName: "key_shares_unique",
+        });
+      });
+  }
 
   //
   // public.pg_dumps
   //
-  await knex.schema
+  const pgDumpsExists = await knex.schema
     .withSchema("public")
-    .hasTable("pg_dumps")
-    .then((exists) => {
-      if (!exists) {
-        return knex.schema
-          .withSchema("public")
-          .createTable("pg_dumps", (table) => {
-            table
-              .uuid("dump_id")
-              .notNullable()
-              .defaultTo(knex.raw("gen_random_uuid()"))
-              .primary({ constraintName: "pg_dumps_pkey" });
-            table.string("status", 16).notNullable();
-            table.string("dump_path", 255);
-            table.jsonb("meta");
-            table
-              .timestamp("created_at", { useTz: true })
-              .notNullable()
-              .defaultTo(knex.fn.now());
-            table
-              .timestamp("updated_at", { useTz: true })
-              .notNullable()
-              .defaultTo(knex.fn.now());
-          });
-      }
-      return Promise.resolve();
+    .hasTable("pg_dumps");
+  if (!pgDumpsExists) {
+    await knex.schema.withSchema("public").createTable("pg_dumps", (table) => {
+      table
+        .uuid("dump_id")
+        .notNullable()
+        .defaultTo(knex.raw("gen_random_uuid()"))
+        .primary({ constraintName: "pg_dumps_pkey" });
+      table.string("status", 16).notNullable();
+      table.string("dump_path", 255);
+      table.jsonb("meta");
+      table
+        .timestamp("created_at", { useTz: true })
+        .notNullable()
+        .defaultTo(knex.fn.now());
+      table
+        .timestamp("updated_at", { useTz: true })
+        .notNullable()
+        .defaultTo(knex.fn.now());
     });
+  }
 
   //
   // public.users
   //
-  await knex.schema
-    .withSchema("public")
-    .hasTable("users")
-    .then((exists) => {
-      if (!exists) {
-        return knex.schema
-          .withSchema("public")
-          .createTable("users", (table) => {
-            table
-              .uuid("user_id")
-              .notNullable()
-              .defaultTo(knex.raw("gen_random_uuid()"))
-              .primary({ constraintName: "users_pkey" });
-            table
-              .string("email", 255)
-              .notNullable()
-              .unique({ indexName: "users_email_key" });
-            table.string("status", 16).notNullable().defaultTo("active");
-            table
-              .timestamp("created_at", { useTz: true })
-              .notNullable()
-              .defaultTo(knex.fn.now());
-            table
-              .timestamp("updated_at", { useTz: true })
-              .notNullable()
-              .defaultTo(knex.fn.now());
-            table.jsonb("aux");
-          });
-      }
-      return Promise.resolve();
+  const usersExists = await knex.schema.withSchema("public").hasTable("users");
+  if (!usersExists) {
+    await knex.schema.withSchema("public").createTable("users", (table) => {
+      table
+        .uuid("user_id")
+        .notNullable()
+        .defaultTo(knex.raw("gen_random_uuid()"))
+        .primary({ constraintName: "users_pkey" });
+      table
+        .string("email", 255)
+        .notNullable()
+        .unique({ indexName: "users_email_key" });
+      table.string("status", 16).notNullable().defaultTo("active");
+      table
+        .timestamp("created_at", { useTz: true })
+        .notNullable()
+        .defaultTo(knex.fn.now());
+      table
+        .timestamp("updated_at", { useTz: true })
+        .notNullable()
+        .defaultTo(knex.fn.now());
+      table.jsonb("aux");
     });
+  }
 
   //
   // public.wallets
   //
-  await knex.schema
+  const walletsExists = await knex.schema
     .withSchema("public")
-    .hasTable("wallets")
-    .then((exists) => {
-      if (!exists) {
-        return knex.schema
-          .withSchema("public")
-          .createTable("wallets", (table) => {
-            table
-              .uuid("wallet_id")
-              .notNullable()
-              .defaultTo(knex.raw("gen_random_uuid()"))
-              .primary({ constraintName: "wallets_pkey" });
-            table.uuid("user_id").notNullable();
-            table.string("curve_type", 16).notNullable();
-            table
-              .binary("public_key")
-              .notNullable()
-              .unique({ indexName: "wallets_public_key_key" });
-            table
-              .timestamp("created_at", { useTz: true })
-              .notNullable()
-              .defaultTo(knex.fn.now());
-            table
-              .timestamp("updated_at", { useTz: true })
-              .notNullable()
-              .defaultTo(knex.fn.now());
-            table.jsonb("aux");
-          });
-      }
-      return Promise.resolve();
+    .hasTable("wallets");
+  if (!walletsExists) {
+    await knex.schema.withSchema("public").createTable("wallets", (table) => {
+      table
+        .uuid("wallet_id")
+        .notNullable()
+        .defaultTo(knex.raw("gen_random_uuid()"))
+        .primary({ constraintName: "wallets_pkey" });
+      table.uuid("user_id").notNullable();
+      table.string("curve_type", 16).notNullable();
+      table
+        .binary("public_key")
+        .notNullable()
+        .unique({ indexName: "wallets_public_key_key" });
+      table
+        .timestamp("created_at", { useTz: true })
+        .notNullable()
+        .defaultTo(knex.fn.now());
+      table
+        .timestamp("updated_at", { useTz: true })
+        .notNullable()
+        .defaultTo(knex.fn.now());
+      table.jsonb("aux");
     });
+  }
 
   //
   // public.server_keypairs
   //
-  await knex.schema
+  const serverKeypairsExists = await knex.schema
     .withSchema("public")
-    .hasTable("server_keypairs")
-    .then((exists) => {
-      if (!exists) {
-        return knex.schema
-          .withSchema("public")
-          .createTable("server_keypairs", (table) => {
-            table
-              .uuid("keypair_id")
-              .notNullable()
-              .defaultTo(knex.raw("gen_random_uuid()"))
-              .primary({ constraintName: "server_keypairs_pkey" });
-            table
-              .specificType("version", "integer generated always as identity")
-              .notNullable()
-              .unique({ indexName: "server_keypairs_version_key" });
-            table.binary("public_key").notNullable();
-            table.text("enc_private_key").notNullable();
-            table.boolean("is_active").notNullable().defaultTo(true);
-            table
-              .timestamp("created_at", { useTz: true })
-              .notNullable()
-              .defaultTo(knex.fn.now());
-            table
-              .timestamp("updated_at", { useTz: true })
-              .notNullable()
-              .defaultTo(knex.fn.now());
-            table.timestamp("rotated_at", { useTz: true });
-          });
-      }
-      return Promise.resolve();
-    });
+    .hasTable("server_keypairs");
+  if (!serverKeypairsExists) {
+    await knex.schema
+      .withSchema("public")
+      .createTable("server_keypairs", (table) => {
+        table
+          .uuid("keypair_id")
+          .notNullable()
+          .defaultTo(knex.raw("gen_random_uuid()"))
+          .primary({ constraintName: "server_keypairs_pkey" });
+        table
+          .specificType("version", "integer generated always as identity")
+          .notNullable()
+          .unique({ indexName: "server_keypairs_version_key" });
+        table.binary("public_key").notNullable();
+        table.text("enc_private_key").notNullable();
+        table.boolean("is_active").notNullable().defaultTo(true);
+        table
+          .timestamp("created_at", { useTz: true })
+          .notNullable()
+          .defaultTo(knex.fn.now());
+        table
+          .timestamp("updated_at", { useTz: true })
+          .notNullable()
+          .defaultTo(knex.fn.now());
+        table.timestamp("rotated_at", { useTz: true });
+      });
+  }
 
   await knex.raw(`
     CREATE INDEX IF NOT EXISTS idx_server_keypairs_is_active
