a53df1537fe1727946416c1b1d4c48674839ba37
Elden Park <eldeniyenden@gmail.com>
2025-12-08 02:35:50 -0800

    o

--- internals/ci/src/main.ts
+++ internals/ci/src/main.ts
@@ -9,19 +9,25 @@ import { dbMigrateKSN } from "./cmds/db_migrate_ksn";
 import { buildCs } from "./cmds/build_cs";
 import { DbSeedAPI } from "./cmds/db_seed_api";
 import { DbMigrateAPI } from "./cmds/db_migrate_api";
+import { langFormat } from "./cmds/lang_format";
+import { langCheck } from "./cmds/lang_check";
 
 async function main() {
   const command = program.version("0.0.1").description("Oko Public CI");
 
   command.command("typecheck").action(typeCheck);
 
-  command.command("build_pkgs").action(buildPkgs);
-
-  command.command("build_sdk").action(buildSDK);
-
   command.command("version").action(version);
 
   command.command("publish").action(publish);
+
+  command.command("lang_format").action(langFormat);
+
+  command.command("lang_check").action(langCheck);
+
+  command.command("build_pkgs").action(buildPkgs);
+
+  command.command("build_sdk").action(buildSDK);
 
   // internals2
   command.command("build_cs").action(buildCs);
