a8714033ce8f3d6974d48b18ab87d4aa17f84751
rettoSea <dbstjd100@naver.com>
2025-12-10 13:54:12 +0900

    oko_attached: fix color-scheme and point-events bug  (#108)
    * [sandbox] add iframe pointer-events none error with mock dapp modal for bug reproduction
    
    * [sdk, attached] add pointer-events'auto'  in iframe
    
    * [attached, sdk_core] fix color-scheme  bug
    
    * [sandbox] add color-scheme toggle
    
    * Publish
    
     - @oko-wallet/customer-dashboard@0.1.2-alpha.3
     - @oko-wallet/demo-web@0.1.2-alpha.3
     - @oko-wallet/email-template-2@0.0.2-alpha.1
     - @oko-wallet/oko-admin-web@0.1.1-alpha.3
     - @oko-wallet/user-dashboard@0.1.1-alpha.2
     - @oko-wallet/bytes@0.0.3-alpha.62
     - @oko-wallet/crypto-js@0.0.2-alpha.6
     - @oko-wallet/cait-sith-keplr-addon@0.0.2-alpha.1
     - @oko-wallet/cait-sith-keplr-hooks@0.1.1-alpha.2
     - @oko-wallet/tecdsa-interface@0.0.2-alpha.26
     - @oko-wallet/oko-attached@0.1.1-alpha.3
     - @oko-wallet-examples/cosmos-nextjs@0.0.2-alpha.1
     - @oko-wallet-examples/cosmos-kit-nextjs@0.0.2-alpha.1
     - @oko-wallet-examples/evm-nextjs@0.0.2-alpha.1
     - @oko-wallet-examples/evm-wagmi-nextjs@0.0.2-alpha.1
     - @oko-wallet-examples/multi-ecosystem-react@0.0.1-alpha.1
     - @oko-wallet/ksn-interface@0.0.2-alpha.58
     - @oko-wallet/key-share-node-server@0.0.5-alpha.16
     - @oko-wallet/dotenv@0.0.2-alpha.27
     - @oko-wallet/stdlib-js@0.0.2-rc.42
     - @oko-wallet/sandbox-cosmos-kit@0.0.2-alpha.1
     - @oko-wallet/oko-cosmos-kit@0.0.1-rc.14
     - @oko-wallet/oko-sdk-core@0.0.6-rc.125
     - @oko-wallet/oko-sdk-cosmos@0.0.6-rc.151
     - @oko-wallet/oko-sdk-eth@0.0.6-rc.140
    
    * [sandbox] delete unused code and file
    
    * [sdk_core] add comment
    
    * [demo, sandbox] bump-up sdks

--- embed/oko_attached/src/components/attached_initialized/color_scheme.ts
+++ embed/oko_attached/src/components/attached_initialized/color_scheme.ts
@@ -8,7 +8,12 @@ export function setColorScheme(theme: Theme) {
   // Set color-scheme to support both light and dark
   // Browser will automatically match parent dApp's color-scheme
   // This prevents browser from adding opaque background
-  root.style.colorScheme = "light dark";
+
+  // NOTE - Applying a color-scheme to the body of an iframe causes a bug where,
+  // if the host site doesn't have a color-scheme and the browser's default is dark,
+  // the background changes to black.
+  // Therefore, we force a light mode in the iframe(in sdk_core not here) @retto
+  // root.style.colorScheme = "light dark";
 }
 
 export async function determineTheme(
