ef47105c6578c99f066c8b6eec2d6cb5e5323168
blacktoast <dbstjd100@naver.com>
2025-12-11 10:41:37 +0900

    [embed] add getSIWEMessage and add condition to show siwe ui

--- embed/oko_attached/src/components/modal_variants/eth/make_sig_eth_modal.tsx
+++ embed/oko_attached/src/components/modal_variants/eth/make_sig_eth_modal.tsx
@@ -1,9 +1,34 @@
 import { type FC } from "react";
 import type { MakeEthereumSigData } from "@oko-wallet/oko-sdk-core";
+import { parseSiweMessage, type SiweMessage } from "viem/siwe";
+import type { SignableMessage } from "viem";
 
 import { MakeArbitrarySigModal } from "./arbitrary_sig/make_arbitrary_sig_modal";
 import { MakeTxSigModal } from "./tx_sig/make_tx_sig_modal";
 import { MakeEIP712SigModal } from "./eip712_sig/make_eip712_sig_modal";
+
+function getSiweMessage(message: SignableMessage): SiweMessage | undefined {
+  if (typeof message !== "string") {
+    return undefined;
+  }
+  const siweMsg = parseSiweMessage(message);
+
+  //NOTE - If any required field in SiweMessage is empty,
+  //  it is determined not to be Siwe.
+  if (
+    siweMsg.address &&
+    siweMsg.chainId &&
+    siweMsg.domain &&
+    siweMsg.version &&
+    siweMsg.nonce &&
+    siweMsg.uri &&
+    siweMsg.address.startsWith("0x")
+  ) {
+    return siweMsg as SiweMessage;
+  }
+
+  return undefined;
+}
 
 export const MakeSignatureEthModal: FC<MakeSignatureEthModalProps> = ({
   getIsAborted,
@@ -21,6 +46,12 @@ export const MakeSignatureEthModal: FC<MakeSignatureEthModalProps> = ({
       );
     }
     case "arbitrary": {
+      const siweMessage = getSiweMessage(data.payload.data.message);
+      const isSiweMessage = !!siweMessage;
+      if (isSiweMessage) {
+        return <>{siweMessage}</>;
+      }
+
       return (
         <MakeArbitrarySigModal
           getIsAborted={getIsAborted}
