e71b7382e08dafab9f6a1904172cefb4ac7ea340
devmosis <ryzond6728@gmail.com>
2025-12-12 16:21:52 +0900

    oko_interchain_kit: remove unused evm codes

--- sdk/oko_interchain_kit/src/init.ts
+++ sdk/oko_interchain_kit/src/init.ts
@@ -1,8 +1,6 @@
 import { OkoCosmosWallet } from "@oko-wallet/oko-sdk-cosmos";
-import { OkoEthWallet } from "@oko-wallet/oko-sdk-eth";
 import type { OkoWalletOptions } from "./types";
 import type { OkoCosmosWalletInterface } from "@oko-wallet/oko-sdk-cosmos";
-import type { OkoEthWalletInterface } from "@oko-wallet/oko-sdk-eth";
 
 export function initializeOkoCosmosWallet(
   options: OkoWalletOptions,
@@ -21,23 +19,4 @@ export function initializeOkoCosmosWallet(
   }
 
   return cosmosWalletResult.data;
-}
-
-export function initializeOkoEthWallet(
-  options: OkoWalletOptions,
-): OkoEthWalletInterface {
-  const ethWalletResult = OkoEthWallet.init({
-    api_key: options.apiKey,
-    sdk_endpoint: options.sdkEndpoint,
-  });
-
-  if (!ethWalletResult.success) {
-    throw new Error(
-      `Failed to initialize OkoEthWallet: ${
-        (ethWalletResult as any).err?.msg || "Unknown error"
-      }`,
-    );
-  }
-
-  return ethWalletResult.data;
 }
