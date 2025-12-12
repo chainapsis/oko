3e001a5112faf7a1f4c36e5d864d50236173a025
Elden Park <eldeniyenden@gmail.com>
2025-12-12 01:45:34 -0800

    o

--- sdk/oko_sdk_eth/rollup.config.ts
+++ sdk/oko_sdk_eth/rollup.config.ts
@@ -2,17 +2,12 @@ import typescript from "@rollup/plugin-typescript";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
-import { dts } from "rollup-plugin-dts";
-import tsConfigPaths from "rollup-plugin-tsconfig-paths";
-// import terser from "@rollup/plugin-terser";
 import type { RollupOptions } from "rollup";
-//
 
 const config: RollupOptions[] = [
   {
     input: "src/index.ts",
     output: {
-      // file: "dist/index.js",
       dir: "dist",
       format: "esm",
       sourcemap: true,
@@ -44,23 +39,6 @@ const config: RollupOptions[] = [
       }),
     ],
   },
-  // {
-  //   input: "src/index.ts",
-  //   output: {
-  //     file: "dist/index.d.ts", // Output path for the bundled declaration file
-  //     format: "esm",
-  //   },
-  //   plugins: [
-  //     tsConfigPaths(),
-  //     json(),
-  //     nodeResolve(),
-  //     dts(),
-  //     typescript({
-  //       emitDeclarationOnly: true,
-  //       exclude: ["**/*.test.ts", "**/tests/**/*", "**/*.spec.ts"],
-  //     }),
-  //   ],
-  // },
 ];
 
 export default config;
