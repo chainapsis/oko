3f73bb4e9c477e9ffb3c577ab968cfaed722c36d
Elden Park <eldeniyenden@gmail.com>
2025-12-15 02:50:57 -0800

    o

--- sdk/oko_sdk_core/scripts/rollup.ts
+++ sdk/oko_sdk_core/scripts/rollup.ts
@@ -6,14 +6,12 @@ import {
   type InputOptions,
   type OutputOptions,
   type RollupBuild,
-  type RollupOptions,
 } from "rollup";
 
 async function main() {
   console.log("Start rollup build");
 
   let bundle: RollupBuild;
-  let buildFailed = false;
 
   const inputOptions: InputOptions = {
     input: "src/index.ts",
@@ -43,12 +41,6 @@ async function main() {
   ];
 
   try {
-    // Create a bundle. If you are using TypeScript or a runtime that
-    // supports it, you can write
-    //
-    // await using bundle = await rollup(inputOptions);
-    //
-    // instead and do not need to close the bundle explicitly below
     bundle = await rollup(inputOptions);
 
     await generateOutputs(bundle, outputOptionsList);
@@ -58,7 +50,6 @@ async function main() {
   }
 
   if (bundle) {
-    // closes the bundle
     await bundle.close();
   }
 }
@@ -68,50 +59,7 @@ async function generateOutputs(
   outputOptionsList: OutputOptions[],
 ) {
   for (const outputOptions of outputOptionsList) {
-    const { output } = await bundle.write(outputOptions);
-    // console.log(44, written.output);
-
-    for (const chunkOrAsset of output) {
-      if (chunkOrAsset.type === "asset") {
-        // For assets, this contains
-        // {
-        //   fileName: string,              // the asset file name
-        //   source: string | Uint8Array    // the asset source
-        //   type: 'asset'                  // signifies that this is an asset
-        // }
-        // console.log("Asset", chunkOrAsset);
-      } else {
-        // For chunks, this contains
-        // {
-        //   code: string,                  // the generated JS code
-        //   dynamicImports: string[],      // external modules imported dynamically by the chunk
-        //   exports: string[],             // exported variable names
-        //   facadeModuleId: string | null, // the id of a module that this chunk corresponds to
-        //   fileName: string,              // the chunk file name
-        //   implicitlyLoadedBefore: string[]; // entries that should only be loaded after this chunk
-        //   imports: string[],             // external modules imported statically by the chunk
-        //   importedBindings: {[imported: string]: string[]} // imported bindings per dependency
-        //   isDynamicEntry: boolean,       // is this chunk a dynamic entry point
-        //   isEntry: boolean,              // is this chunk a static entry point
-        //   isImplicitEntry: boolean,      // should this chunk only be loaded after other chunks
-        //   map: string | null,            // sourcemaps if present
-        //   modules: {                     // information about the modules in this chunk
-        //     [id: string]: {
-        //       renderedExports: string[]; // exported variable names that were included
-        //       removedExports: string[];  // exported variable names that were removed
-        //       renderedLength: number;    // the length of the remaining code in this module
-        //       originalLength: number;    // the original length of the code in this module
-        //       code: string | null;       // remaining code in this module
-        //     };
-        //   },
-        //   name: string                   // the name of this chunk as used in naming patterns
-        //   preliminaryFileName: string    // the preliminary file name of this chunk with hash placeholders
-        //   referencedFiles: string[]      // files referenced via import.meta.ROLLUP_FILE_URL_<id>
-        //   type: 'chunk',                 // signifies that this is a chunk
-        // }
-        // console.log("Chunk", chunkOrAsset.modules);
-      }
-    }
+    await bundle.write(outputOptions);
   }
 }
 
