27c261e4ddd272ec7a4539f1f63cf0c4eb671bba
lidarbtc <lidarbtc@protonmail.com>
2025-12-15 16:38:33 +0900

    o

--- sandbox/sdk_version_test/README.md
+++
@@ -1,9 +0,0 @@
-# Oko Sandbox Simple Host
-
-This is a simple sandbox that allows you to test the Oko SDK in a simple host environment.
-
-## How to run
-
-```bash
-yarn dev
-```
--- sandbox/sdk_version_test/src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const TEST_COSMOS_CHAIN_ID = "osmosis-1";
-export const TEST_COSMOS_CHAIN_RPC = "https://osmosis-rpc.publicnode.com:443";
-export const TEST_COSMOS_CHAIN_REST = "https://osmosis-rest.publicnode.com";
-export const TEST_COSMOS_CHAIN_ENDPOINT = "/cosmos/tx/v1beta1/txs";
