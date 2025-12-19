6ff7e1f4bcb28d3adfa0fa88945f507ffc770e30
devmosis <ryzond6728@gmail.com>
2025-12-19 16:56:01 +0900

    Revert "backend/tss_api: add ed25519 keygen and sign endpoints"
    This reverts commit 6be157cc9c3dad264fe2db14077036319ab37cc8.

--- backend/tss_api/src/requests/index.ts
+++ backend/tss_api/src/requests/index.ts
@@ -1,4 +1,4 @@
-import type { Bytes32, Bytes33 } from "@oko-wallet/bytes";
+import type { Bytes33 } from "@oko-wallet/bytes";
 import type { OkoApiResponse } from "@oko-wallet/oko-types/api_response";
 import type { CheckKeyShareResponse } from "@oko-wallet/ksn-interface/key_share";
 import type { AuthType } from "@oko-wallet/oko-types/auth";
@@ -18,25 +18,6 @@ export async function requestCheckKeyShare(
       email: userEmail,
       auth_type,
       public_key: publicKey.toHex(),
-    }),
-  });
-  return (await res.json()) as OkoApiResponse<CheckKeyShareResponse>;
-}
-
-export async function requestCheckKeyShareEd25519(
-  ksNodeURI: string,
-  userEmail: string,
-  publicKey: Bytes32,
-) {
-  const res = await fetch(`${ksNodeURI}/keyshare/v1/check_ed25519`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email: userEmail,
-      public_key: publicKey.toHex(),
-      curve_type: "ed25519",
     }),
   });
   return (await res.json()) as OkoApiResponse<CheckKeyShareResponse>;
