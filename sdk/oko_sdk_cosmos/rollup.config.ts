95bc1feb7c7328eed9444587a6d81a691b397508
Elden Park <eldeniyenden@gmail.com>
2025-12-22 00:56:17 -0800

    o

--- sdk/oko_sdk_cosmos/rollup.config.ts
+++ sdk/oko_sdk_cosmos/rollup.config.ts
@@ -1,41 +1,41 @@
-import typescript from "@rollup/plugin-typescript";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import json from "@rollup/plugin-json";
-import type { RollupOptions } from "rollup";
-
-export const config: RollupOptions[] = [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        dir: "dist",
-        format: "esm",
-        sourcemap: true,
-      },
-    ],
-    external: [
-      "@oko-wallet/oko-sdk-core",
-      "@cosmjs/amino",
-      "@cosmjs/proto-signing",
-      "@oko-wallet/stdlib-js",
-      "@keplr-wallet/proto-types",
-      "@keplr-wallet/types",
-      "@noble/curves",
-      "@noble/hashes",
-      "bech32",
-      "buffer",
-    ],
-    plugins: [
-      json(),
-      nodeResolve(),
-      commonjs(),
-      typescript({
-        tsconfig: "./tsconfig.rollup.json",
-        noEmitOnError: true,
-      }),
-    ],
-  },
-];
-
-export default config;
+// import typescript from "@rollup/plugin-typescript";
+// import { nodeResolve } from "@rollup/plugin-node-resolve";
+// import commonjs from "@rollup/plugin-commonjs";
+// import json from "@rollup/plugin-json";
+// import type { RollupOptions } from "rollup";
+//
+// export const config: RollupOptions[] = [
+//   {
+//     input: "src/index.ts",
+//     output: [
+//       {
+//         dir: "dist",
+//         format: "esm",
+//         sourcemap: true,
+//       },
+//     ],
+//     external: [
+//       "@oko-wallet/oko-sdk-core",
+//       "@cosmjs/amino",
+//       "@cosmjs/proto-signing",
+//       "@oko-wallet/stdlib-js",
+//       "@keplr-wallet/proto-types",
+//       "@keplr-wallet/types",
+//       "@noble/curves",
+//       "@noble/hashes",
+//       "bech32",
+//       "buffer",
+//     ],
+//     plugins: [
+//       json(),
+//       nodeResolve(),
+//       commonjs(),
+//       typescript({
+//         tsconfig: "./tsconfig.rollup.json",
+//         noEmitOnError: true,
+//       }),
+//     ],
+//   },
+// ];
+//
+// export default config;
