021d94880065da412f037f3963bfa2e0b5623c38
Elden Park <eldeniyenden@gmail.com>
2025-12-22 08:53:53 -0800

    email_template: remove legacy email_template in favor of email_template_2

--- apps/email_template/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  presets: [require("tailwindcss-preset-email")],
-  content: [
-    "./components/**/*.html",
-    "./emails/**/*.html",
-    "./layouts/**/*.html",
-  ],
-};
