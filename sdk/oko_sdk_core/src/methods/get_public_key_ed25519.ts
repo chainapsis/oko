872b5ceaa2b47f215ddca189253f073b3b59092e
devmosis <ryzond6728@gmail.com>
2025-12-24 03:48:56 +0900

    oko-sdk-core: add Ed25519 public key method

---
+++ sdk/oko_sdk_core/src/methods/get_public_key_ed25519.ts
@@ -0,0 +1,20 @@
+import type { OkoWalletInterface } from "@oko-wallet-sdk-core/types";
+import { OKO_ATTACHED_TARGET } from "@oko-wallet-sdk-core/window_msg/target";
+
+export async function getPublicKeyEd25519(
+  this: OkoWalletInterface,
+): Promise<string | null> {
+  await this.waitUntilInitialized;
+
+  const res = await this.sendMsgToIframe({
+    target: OKO_ATTACHED_TARGET,
+    msg_type: "get_public_key_ed25519",
+    payload: null,
+  });
+
+  if (res.msg_type === "get_public_key_ed25519_ack" && res.payload.success) {
+    return res.payload.data;
+  }
+
+  return null;
+}
