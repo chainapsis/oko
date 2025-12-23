a998d8f30a2c5aba67273b835e4d59cb24e299e1
devmosis <ryzond6728@gmail.com>
2025-12-24 03:45:50 +0900

    oko_attached: add Ed25519 keygen support

--- embed/oko_attached/src/wasm/index.ts
+++ embed/oko_attached/src/wasm/index.ts
@@ -1,11 +1,23 @@
-import * as wasmModule from "@oko-wallet/cait-sith-keplr-wasm/pkg/cait_sith_keplr_wasm";
-import { initWasm } from "@oko-wallet/cait-sith-keplr-wasm";
+import * as caitSithWasmModule from "@oko-wallet/cait-sith-keplr-wasm/pkg/cait_sith_keplr_wasm";
+import { initWasm as initCaitSithWasm } from "@oko-wallet/cait-sith-keplr-wasm";
+import * as teddsaWasmModule from "@oko-wallet/teddsa-wasm/pkg/teddsa_wasm";
+import { initWasm as initTeddsaWasm } from "@oko-wallet/teddsa-wasm";
 
 export async function initKeplrWasm() {
   try {
-    await initWasm(wasmModule, "/pkg/cait_sith_keplr_wasm_bg.wasm");
-    console.log("[attached] WASM initialized");
+    await initCaitSithWasm(
+      caitSithWasmModule,
+      "/pkg/cait_sith_keplr_wasm_bg.wasm",
+    );
+    console.log("[attached] cait-sith WASM initialized");
   } catch (err) {
-    console.error("[attached] Error initializing WASM, err: %s", err);
+    console.error("[attached] Error initializing cait-sith WASM, err: %s", err);
+  }
+
+  try {
+    await initTeddsaWasm(teddsaWasmModule, "/pkg/teddsa_wasm_bg.wasm");
+    console.log("[attached] teddsa WASM initialized");
+  } catch (err) {
+    console.error("[attached] Error initializing teddsa WASM, err: %s", err);
   }
 }
