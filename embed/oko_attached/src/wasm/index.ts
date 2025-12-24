954f270ed0d64eee5af556becbde1cbd9568dde1
devmosis <ryzond6728@gmail.com>
2025-12-24 22:12:27 +0900

    teddsa: replace teddsa-wasm with frost-ed25519-keplr-wasm

--- embed/oko_attached/src/wasm/index.ts
+++ embed/oko_attached/src/wasm/index.ts
@@ -1,7 +1,7 @@
 import * as caitSithWasmModule from "@oko-wallet/cait-sith-keplr-wasm/pkg/cait_sith_keplr_wasm";
 import { initWasm as initCaitSithWasm } from "@oko-wallet/cait-sith-keplr-wasm";
-import * as teddsaWasmModule from "@oko-wallet/teddsa-wasm/pkg/teddsa_wasm";
-import { initWasm as initTeddsaWasm } from "@oko-wallet/teddsa-wasm";
+import * as frostWasmModule from "@oko-wallet/frost-ed25519-keplr-wasm/pkg/frost_ed25519_keplr_wasm";
+import { initWasm as initFrostWasm } from "@oko-wallet/frost-ed25519-keplr-wasm";
 
 export async function initKeplrWasm() {
   try {
@@ -15,9 +15,9 @@ export async function initKeplrWasm() {
   }
 
   try {
-    await initTeddsaWasm(teddsaWasmModule, "/pkg/teddsa_wasm_bg.wasm");
-    console.log("[attached] teddsa WASM initialized");
+    await initFrostWasm(frostWasmModule, "/pkg/frost_ed25519_keplr_wasm_bg.wasm");
+    console.log("[attached] frost-ed25519 WASM initialized");
   } catch (err) {
-    console.error("[attached] Error initializing teddsa WASM, err: %s", err);
+    console.error("[attached] Error initializing frost-ed25519 WASM, err: %s", err);
   }
 }
