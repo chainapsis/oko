88120ed6931fa38bb3bf4f8ca9956c04eeabd1b1
Calvin Lee <jinwoo0451@naver.com>
2025-12-29 18:01:57 +0900

    teddsa: refactor CentralizedKeygenOutputRaw

--- crypto/teddsa/frost_ed25519_keplr_wasm/wasm/src/keys/key_package.rs
+++ crypto/teddsa/frost_ed25519_keplr_wasm/wasm/src/keys/key_package.rs
@@ -5,17 +5,11 @@ use serde::{Deserialize, Serialize};
 use super::PublicKeyPackageRaw;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct KeygenOutputRaw {
-    pub identifier: [u8; 32],
-    pub key_package: KeyPackageRaw,
-    pub public_key_package: PublicKeyPackageRaw,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct CentralizedKeygenOutputRaw {
     pub private_key: [u8; 32],
     pub public_key: [u8; 32],
-    pub keygen_outputs: Vec<KeygenOutputRaw>,
+    pub keygen_outputs: Vec<KeyPackageRaw>,
+    pub public_key_package: PublicKeyPackageRaw,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
