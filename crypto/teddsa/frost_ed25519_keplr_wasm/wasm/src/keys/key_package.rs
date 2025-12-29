ba23b17e6fa2651146b017864175132cdeec0d97
Calvin Lee <jinwoo0451@naver.com>
2025-12-29 17:32:33 +0900

    teddsa: impl from_key_package method

--- crypto/teddsa/frost_ed25519_keplr_wasm/wasm/src/keys/key_package.rs
+++ crypto/teddsa/frost_ed25519_keplr_wasm/wasm/src/keys/key_package.rs
@@ -1,3 +1,4 @@
+use frost_ed25519_keplr::keys::KeyPackage;
 use serde::{Deserialize, Serialize};
 
 use super::PublicKeyPackageRaw;
@@ -23,4 +24,42 @@ pub struct KeyPackageRaw {
     pub verifying_share: [u8; 32],
     pub verifying_key: [u8; 32],
     pub min_signers: u16,
+}
+
+impl KeyPackageRaw {
+    pub fn from_key_package(pkg: &KeyPackage) -> Result<Self, String> {
+        let identifier: [u8; 32] = pkg
+            .identifier()
+            .serialize()
+            .try_into()
+            .map_err(|_| "Invalid identifier length")?;
+
+        let signing_share: [u8; 32] = pkg
+            .signing_share()
+            .serialize()
+            .try_into()
+            .map_err(|_| "Invalid signing share length")?;
+
+        let verifying_share: [u8; 32] = pkg
+            .verifying_share()
+            .serialize()
+            .map_err(|e| e.to_string())?
+            .try_into()
+            .map_err(|_| "Invalid verifying share length")?;
+
+        let verifying_key: [u8; 32] = pkg
+            .verifying_key()
+            .serialize()
+            .map_err(|e| e.to_string())?
+            .try_into()
+            .map_err(|_| "Invalid verifying key length")?;
+
+        Ok(Self {
+            identifier,
+            signing_share,
+            verifying_share,
+            verifying_key,
+            min_signers: *pkg.min_signers(),
+        })
+    }
 }
