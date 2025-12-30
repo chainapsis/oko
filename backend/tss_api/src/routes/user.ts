1528e2956a7e353f4f52e59ae216e07cf5612f3d
chihunmanse <chihunmanse@gmail.com>
2025-12-30 18:34:23 +0900

    oko_api: replace email with user_identifier in reshare

--- backend/tss_api/src/routes/user.ts
+++ backend/tss_api/src/routes/user.ts
@@ -413,13 +413,14 @@ export function setUserRoutes(router: Router) {
       const state = req.app.locals;
       const oauthUser = res.locals.oauth_user;
       const auth_type = oauthUser.type as AuthType;
+      const user_identifier = oauthUser.user_identifier;
       const { public_key, reshared_key_shares } = req.body;
 
-      if (!oauthUser?.email) {
+      if (!user_identifier) {
         res.status(401).json({
           success: false,
           code: "UNAUTHORIZED",
-          msg: "User email not found",
+          msg: "User identifier not found",
         });
         return;
       }
@@ -445,7 +446,7 @@ export function setUserRoutes(router: Router) {
 
       const reshareRes = await updateWalletKSNodesForReshare(
         state.db,
-        oauthUser.email.toLowerCase(),
+        user_identifier,
         auth_type,
         publicKeyRes.data,
         reshared_key_shares,
