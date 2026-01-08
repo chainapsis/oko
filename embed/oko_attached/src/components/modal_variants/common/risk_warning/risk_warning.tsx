0c3c648cead11a3366ff9cadd89229635b5c5a48
Dan <ryzond6728@gmail.com>
2026-01-08 16:24:30 +0900

    oko_attached: impl "sign-in-with-solana" preliminary
    * oko_attached: implement siws modal
    
    * oko_attached: extract common sign-in verification logic for SIWE/SIWS
    
    * oko_attached: consolidate SIWE/SIWS common components
    
    * oko_attached: consolidate SIWE/SIWS content styles
    
    * oko_attached: remove unused codes
    
    ---------
    
    Co-authored-by: Calvin Lee <jinwoo0451@naver.com>

--- embed/oko_attached/src/components/modal_variants/common/risk_warning/risk_warning.tsx
+++ embed/oko_attached/src/components/modal_variants/common/risk_warning/risk_warning.tsx
@@ -2,20 +2,21 @@ import type { FC } from "react";
 import { Checkbox } from "@oko-wallet/oko-common-ui/checkbox";
 import { Typography } from "@oko-wallet/oko-common-ui/typography";
 
-import styles from "./siwe_risk_warning_box.module.scss";
+import styles from "./risk_warning.module.scss";
 
-interface SiweRiskWarningCheckBoxProps {
+interface RiskWarningCheckBoxProps {
   checked: boolean;
-  onChange: (isValidSiweMessage: boolean) => void;
+  onChange: (checked: boolean) => void;
 }
-export const SiweRiskWarningCheckBox: FC<SiweRiskWarningCheckBoxProps> = ({
+
+export const RiskWarningCheckBox: FC<RiskWarningCheckBoxProps> = ({
   checked,
   onChange,
 }) => {
   return (
-    <div className={styles.siweRiskWarningContainer}>
+    <div className={styles.riskWarningContainer}>
       <Checkbox
-        id="siwe-message-checkbox"
+        id="sign-in-message-checkbox"
         checked={checked}
         onChange={onChange}
         label="I understand the risk and would like to proceed."
@@ -24,18 +25,18 @@ export const SiweRiskWarningCheckBox: FC<SiweRiskWarningCheckBoxProps> = ({
           size: "xs",
           weight: "medium",
         }}
-        checkboxContainerClassName={styles.siweRiskWarningCheckBoxContainer}
+        checkboxContainerClassName={styles.riskWarningCheckBoxContainer}
         checkBoxInputContainerClassName={
-          styles.siweRiskWarningCheckBoxInputContainerClassName
+          styles.riskWarningCheckBoxInputContainerClassName
         }
       />
     </div>
   );
 };
 
-export const SiweRiskWarningBox = () => {
+export const RiskWarningBox: FC = () => {
   return (
-    <div className={styles.siweRiskWarningBox}>
+    <div className={styles.riskWarningBox}>
       <Typography size="xs" color="warning-primary" weight="medium">
         The URL requesting your signature differs from the current website. This
         may indicate a potential security risk.
