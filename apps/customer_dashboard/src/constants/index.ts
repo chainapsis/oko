dda4da6b95b38f5e51e83da3301459e8f307cf43
lidarbtc <lidarbtc@protonmail.com>
2026-01-08 18:47:35 +0900

    customer_dashboard: Hardcode email verification timer to 60

--- apps/customer_dashboard/src/constants/index.ts
+++ apps/customer_dashboard/src/constants/index.ts
@@ -3,8 +3,6 @@ export const PASSWORD_MIN_LENGTH = 8;
 
 export const SIX_DIGITS_REGEX = /^\d{6}$/;
 
-export const EMAIL_VERIFICATION_TIMER_SECONDS = Number(
-  process.env.NEXT_PUBLIC_EMAIL_VERIFICATION_TIMER_SECONDS,
-);
+export const EMAIL_VERIFICATION_TIMER_SECONDS = 60;
 
 export const GET_STARTED_URL = "https://form.typeform.com/to/MxrBGq9b";
