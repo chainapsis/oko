7c776d1c950fc0d72c076b3cfbb679e5873d9bcd
Hyunjae Chung <lidarbtc@protonmail.com>
2026-01-14 15:46:46 +0900

    customer_dashboard: reset password when it is lost
    * customer_dashboard: Make email timer configurable
    
    * customer_dashboard: Limit forgot password inputs to 16 chars
    
    * customer_dashboard: Hardcode email verification timer to 60
    
    * oko_common_ui: fix checkbox icon not visible when data-theme is not set

--- apps/customer_dashboard/src/constants/index.ts
+++ apps/customer_dashboard/src/constants/index.ts
@@ -1,8 +1,10 @@
 export const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 export const PASSWORD_MIN_LENGTH = 8;
+export const PASSWORD_MAX_LENGTH = 16;
+export const PASSWORD_CONTAINS_NUMBER_REGEX = /\d/;
 
 export const SIX_DIGITS_REGEX = /^\d{6}$/;
 
-export const EMAIL_VERIFICATION_TIMER_SECONDS = 60 * 3;
+export const EMAIL_VERIFICATION_TIMER_SECONDS = 60;
 
 export const GET_STARTED_URL = "https://form.typeform.com/to/MxrBGq9b";
