3f6b4156f084e277a2dad9d7c46ed0cd545ecd69
Elden Park <eldeniyenden@gmail.com>
2026-01-15 20:11:14 -0800

    o

--- internals/ci/src/cmds/lang_lint.ts
+++ internals/ci/src/cmds/lang_lint.ts
@@ -1,10 +1,10 @@
 import { spawnSync } from "node:child_process";
 
-import { paths } from "@oko-wallet-ci/paths";
 import { expectSuccess } from "@oko-wallet-ci/expect";
+import { paths } from "@oko-wallet-ci/paths";
 
-export async function langLint(..._args: any[]) {
-  console.log("Checking language of codebase...");
+export async function langLint(...args: any) {
+  console.log("Checking language of codebase...", args);
 
   const publishRet = spawnSync("yarn", ["exec", "biome", "check", "--write"], {
     cwd: paths.root,
