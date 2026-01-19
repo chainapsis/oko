aab3f7fa27049b6037a6bd7c302694dfd174dba2
Elden Park <eldeniyenden@gmail.com>
2026-01-18 21:45:30 -0800

    o

--- sandbox/sandbox_react_native/app/index.tsx
+++ sandbox/sandbox_react_native/app/index.tsx
@@ -2,7 +2,8 @@ import { Text, View } from "react-native";
 import { WebView } from "react-native-webview";
 
 export default function Index() {
-  const sandboxURL = "http://10.0.2.2:4200";
+  // const sandboxURL = "http://10.0.2.2:4200";
+  const sandboxURL = "https://reactnative.dev";
   console.log(1231, sandboxURL);
 
   fetch(sandboxURL).then((res) => console.log(44, res));
@@ -23,7 +24,7 @@ export default function Index() {
         hello world
       </Text>
       <WebView
-        source={{ uri: sandboxURL }}
+        source={{ uri: "https://reactnative.dev" }}
         style={{ width: "100%", borderWidth: 2, borderColor: "black" }}
       />
     </View>
