6e67c6dd58d9f1be070ed4c1d03e6510f1288a89
Elden Park <eldeniyenden@gmail.com>
2026-01-18 21:04:08 -0800

    o

--- sandbox/sandbox_react_native/app/_layout.tsx
+++ sandbox/sandbox_react_native/app/_layout.tsx
@@ -1,29 +1,5 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
-  const url = 'http://10.0.2.2:4200'
-  console.log(123, url);
-
-  fetch(url).then((res) => console.log(res));
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+  return <Stack />;
 }
