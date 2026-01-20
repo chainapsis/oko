bdbf663337786a92e415adc0c0f18dcd39656bbe
lidarbtc <lidarbtc@protonmail.com>
2026-01-20 17:51:42 +0900

    o

--- sandbox/sandbox_react_native/app/index.tsx
+++ sandbox/sandbox_react_native/app/index.tsx
@@ -2,6 +2,7 @@ import { Text, View } from "react-native";
 import { WebView } from "react-native-webview";
 
 export default function Index() {
+  // temp
   // const sandboxURL = "http://10.0.2.2:4200";
   const sandboxURL = "https://reactnative.dev";
   console.log("url", sandboxURL);
