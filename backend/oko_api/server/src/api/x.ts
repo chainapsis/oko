adb642449748fffac3461b4d5bf48f6d5e26bc08
chihunmanse <chihunmanse@gmail.com>
2026-01-23 14:52:56 +0900

    o

--- backend/oko_api/server/src/api/x.ts
+++ backend/oko_api/server/src/api/x.ts
@@ -1,8 +1,6 @@
 import type { Result } from "@oko-wallet/stdlib-js";
 import type { SocialLoginXVerifyUserResponse } from "@oko-wallet/oko-types/social_login";
 
-// import { X_USER_INFO_URL } from "@oko-wallet/social-login-api/src/constants/x";
-
 export const X_SOCIAL_LOGIN_TOKEN_URL = "https://api.x.com/2/oauth2/token";
 export const X_USER_INFO_URL = "https://api.x.com/2/users/me";
 export const X_CLIENT_ID = "eWJPdVNYNlV6dEpNSTM3T01GRGI6MTpjaQ";
