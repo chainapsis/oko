81dac6c07f4c88b2370700c5c008b07bb051f917
Elden Park <eldeniyenden@gmail.com>
2026-01-28 00:29:29 -0800

    o

--- internals/github/deploy_oko_apps.sh
+++ internals/github/deploy_oko_apps.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 act --workflows "../../.github/workflows/deploy_oko_apps.yml" \
+    --input tag=develop/v0.0.1
     --secret-file .secrets \
     --var-file .vars \
+    -s ACTIONS_STEP_DEBUG=true \
     --container-architecture linux/amd64
